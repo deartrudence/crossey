cbb3080e78f4fe56515d7184bb895f1dc68c9605
deartrudence <trudy.macnabb@gmail.com>
2016-10-10 08:01:52 -0400

    added section comments to review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   # before_action :authenticate_has_profile, unless: :devise_controller?
   
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, :alert => exception.message
+  end
   
   def authenticate_has_profile
     if !current_user.profile.present?
