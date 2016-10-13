b7ca9a34f6fb39aebb98cc910ddfb07290974bd7
deartrudence <trudy.macnabb@gmail.com>
2016-10-13 11:25:14 -0400

    styling and 404 page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -12,17 +12,17 @@ class Ability
         can [:manage], IndividualReview
         user_array = user.authored_reviews.map(&:employee_id)
         users = User.where(id: user_array)
-        can [:update, :edit, :show, :index], Profile do |profile|
+        can [:update, :edit, :show], Profile do |profile|
             user_array.include?(profile.user_id)
         end
     elsif user.is_reviewer? 
         can [:create], IndividualReview
         can [:update, :edit, :show, :index, :download], IndividualReview, :reviewer_id => user.id
         can [:update, :edit, :show, :index], IndividualReview, :employee_id => user.id
-        can [:update, :edit, :show, :index], Profile, :user_id => user.id
+        can [:update, :edit, :show], Profile, :user_id => user.id
         user_array = user.authored_reviews.map(&:employee_id)
         users = User.where(id: user_array)
-        can [:update, :edit, :show, :index], Profile do |profile|
+        can [:update, :edit, :show], Profile do |profile|
             user_array.include?(profile.user_id)
         end
     elsif user.is_employee?
