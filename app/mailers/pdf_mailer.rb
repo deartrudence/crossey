0b4164fa3be557eb8f72be710202dd7bfd5c9fa1
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-10-14 09:58:03 -0400

    update to mailer

--- app/mailers/pdf_mailer.rb
+++ app/mailers/pdf_mailer.rb
@@ -1,6 +1,8 @@
 class PdfMailer < ApplicationMailer
 	require 'mailgun'
-	def pdf_email
+	def pdf_email(pdf, employee)
+		
+		attachments["review-#{employee.name}.pdf"] = pdf
 		mail(
 			to: 'hello@upatfive.ca', 
 			subject: 'New Review'
