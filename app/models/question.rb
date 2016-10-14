9644dc6f8b610cf7fb47c1e46a7d983a152cefdf
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-10-14 15:35:44 -0400

    update queries in scoes for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,8 @@ class Question < ActiveRecord::Base
   belongs_to :section
   has_many :answers
 
-  scope :question_belongs_to_job_level, -> (user_level) { where('job_type = ?', user_level.to_i) }
-  scope :question_belongs_to_next_job_level, -> (user_level) { where('job_type = ?', user_level.to_i + 1) }
+  scope :question_belongs_to_job_level, -> (user_level) { where(job_type: user_level.to_i) }
+  scope :question_belongs_to_next_job_level, -> (user_level) { where(job_type: user_level.to_i + 1) }
 
   def belongs_to_job_level?(user)
   	user_job_level = user.profile.job_level
