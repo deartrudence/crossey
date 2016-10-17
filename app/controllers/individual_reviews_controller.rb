41d86b423c924710b7e88a8a19ce32a32b3cb1f4
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-10-17 17:31:04 -0400

    add employee job level

--- app/controllers/individual_reviews_controller.rb
+++ app/controllers/individual_reviews_controller.rb
@@ -62,7 +62,7 @@ class IndividualReviewsController < ApplicationController
     employee = Profile.where(id: individual_review_params[:employee_id]).first
     @individual_review = IndividualReview.new(individual_review_params)
     @individual_review.reviewer_id = current_user.id
-    @individual_review.employee_job_level = employee.job_level
+    @individual_review.employee_job_level = employee.job_level.to_i
     @individual_review.employee_job_title = employee.job_title
     respond_to do |format|
       if @individual_review.save
