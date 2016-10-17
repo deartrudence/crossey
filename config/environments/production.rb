f0211ba7c4134ad5020f5ef7fefd862ac0c9b658
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-10-17 17:11:39 -0400

    update to mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,14 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :domain         => ENV['domain'],
+    :user_name      => ENV['username'],
+    :password       => ENV['password'],
+    :authentication => :plain,
+  }
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
