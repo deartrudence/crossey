eba69c6d9b43d23b5a1e1be31b6d7d9b7b4e930c
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-10-17 17:43:50 -0400

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
     :address        => "smtp.mailgun.org",
-    :domain         => ENV['domain'],
+    # :domain         => ENV['domain'],
     :user_name      => ENV['username'],
     :password       => ENV['password'],
     :authentication => :plain,
