4e01400cf4287954113491972b9a80f83175f70f
deartrudence <trudy.macnabb@gmail.com>
2016-10-28 10:44:42 -0400

    remove registratible from user model devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
-
+# :registerable,
   def is_employee?
     if self.profile.present?
   	 self.profile.user_roles.map(&:name).include?('Employee')
