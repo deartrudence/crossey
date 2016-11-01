a4d7949d9988b327d9b9a7a0845c197027e64272
deartrudence <trudy.macnabb@gmail.com>
2016-11-01 11:31:47 -0400

    added registrable back to devise for user sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :registerable
 # :registerable,
   def is_employee?
     if self.profile.present?
