195aca5062df2944eb5903d11befae6c54e3aee4
Jordan Deutsch <jordanbdeutsch@gmail.com>
2016-12-01 09:26:41 -0500

    remove splash

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   resources :reviews
   devise_for :users
 
-  root 'pages#maintenance'
+  root 'individual_reviews#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
